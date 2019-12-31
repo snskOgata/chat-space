84d61664a988d640819c03022d33b463651ab879
Shunsuke Ogata <snsk.ogata@gmail.com>
2019-12-31 10:54:13 +0900

    Add views for group

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resouces :groups, except: [:index, :destroy]
+  resources :groups, except: [:index, :destroy]
   root "messages#index"
 end
