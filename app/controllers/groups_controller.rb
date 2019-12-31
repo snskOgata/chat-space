b7e3dac26c0c95f14c22d1e1f20d7aeb5b31aa87
Shunsuke Ogata <snsk.ogata@gmail.com>
2019-12-31 11:57:55 +0900

    Make new and create action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
-  
+
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -14,5 +22,10 @@ class GroupsController < ApplicationController
 
   def update
   end
+
+  private
+    def group_params
+      params.require(:group).permit(:name, user_ids: [])
+    end
 
 end
