fec8ffaf724d733001eb61891c108ef02307a16f
Shunsuke Ogata <snsk.ogata@gmail.com>
2020-01-03 12:45:30 +0900

    Show user-list with typed-keyword

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,23 @@
 $(function () {
 
+  function appendSearchedUserlist(users) {
+    html = ""
+    if (users.length) {
+      $.each(users, function (i, user) {
+        html += `<div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">${user.name}</p>
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+              </div>`
+      });
+    }
+    else {
+      html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">ユーザーが見つかりません</p>
+               </div>`
+    }
+    $("#user-search-result").append(html);
+  }
+
   $("#user-search-field").on("keyup", function () {
     var input = $("#user-search-field").val();
 
@@ -8,7 +26,13 @@ $(function () {
       url: '/users',
       data: { keyword: input },
       dataType: 'json'
-    });
+    })
+      .done(function (users) {
+        $("#user-search-result").empty();
+        appendSearchedUserlist(users);
+      })
+      .fail(function () {
+        console.log('エラー');
+      });
   });
-
 });
